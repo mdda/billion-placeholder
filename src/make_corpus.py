d3716edc169ed3f6ce5d9b08dd8fa57a51929289
Martin Andrews <GitHub@mdda.net>
2014-10-09 18:37:42 +0800

    argparse with lines

--- src/make_corpus.py
+++ src/make_corpus.py
@@ -5,9 +5,12 @@ import argparse
 parser = argparse.ArgumentParser(description='Parses training file in to Corpus (for GloVe)')
 parser.add_argument('-i','--input', help='Input file name', required=True)
 parser.add_argument('-o','--output', help='Output file name', required=True)
+parser.add_argument('-l','--lines', help='# of lines', type=int, default=-1)
 
 args = parser.parse_args()
 
 ## show values ##
 print ("Input file: %s" % args.input )
 print ("Output file: %s" % args.output )
+print ("Lines : %d" % args.lines )
+
