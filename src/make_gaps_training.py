ec81d6769504682865664428cbb64fa3723465be
Martin Andrews <GitHub@mdda.net>
2015-01-03 08:00:03 +0800

    Small adjustments

--- src/make_gaps_training.py
+++ src/make_gaps_training.py
@@ -6,7 +6,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Converts corpus to "gaps training data"')
 parser.add_argument('-i','--input', help='Input file name', required=True)
-parser.add_argument('--vocab', help='Vocab file name', required=True)
+parser.add_argument(     '--vocab', help='Vocab file name', required=True)
 parser.add_argument('-o','--output', help='Output file name', required=True)
 
 args = parser.parse_args()
