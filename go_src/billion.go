72735b78e8ef9fad42627cfaa29e39bfce6159b2
Martin Andrews <GitHub@mdda.net>
2015-04-18 17:23:37 +0800

    Re-Tab

--- go_src/billion.go
+++ go_src/billion.go
@@ -3,10 +3,10 @@ package main
 // GOPATH=`pwd` go build billion.go  && ./billion
 
 import (
-	"fmt"
-	"time"
 	"flag"
+	"fmt"
 	"math/rand"
+	"time"
 )
 
 import (
@@ -14,11 +14,11 @@ import (
 )
 
 import (
-  "os"
-  "io"
 	"encoding/csv"
-//	"strconv"
-  "strings"
+	"io"
+	"os"
+	//	"strconv"
+	"strings"
 )
 
 import (
@@ -27,26 +27,27 @@ import (
 
 // A data structure to hold a key/value pair.
 type Pair struct {
-  Key string
-  Value int
+	Key   string
+	Value int
 }
 
 // A slice of Pairs that implements sort.Interface to sort by Value.
 type PairList []Pair
-func (p PairList) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
-func (p PairList) Len() int { return len(p) }
+
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Len() int           { return len(p) }
 func (p PairList) Less(i, j int) bool { return p[i].Value > p[j].Value } // Sort DESC
 
-// A function to turn a map into a PairList, then sort and return it. 
+// A function to turn a map into a PairList, then sort and return it.
 func sortMapByValue(m map[string]int) PairList {
-   p := make(PairList, len(m))
-   i := 0
-   for k, v := range m {
-      p[i] = Pair{k, v}
-      i++
-   }
-   sort.Sort(p)
-   return p
+	p := make(PairList, len(m))
+	i := 0
+	for k, v := range m {
+		p[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(p)
+	return p
 }
 
 func read_test_ngram(filename string, n int) PairList {
@@ -67,16 +68,16 @@ func read_test_ngram(filename string, n int) PairList {
 		return PairList{}
 	}
 
-/*
-	id_max := 0
-	id_map := make(map[int]bool)
-	for _, id := range id_list {
-		id_map[id] = true
-		if id_max < id {
-			id_max = id
+	/*
+		id_max := 0
+		id_map := make(map[int]bool)
+		for _, id := range id_list {
+			id_map[id] = true
+			if id_max < id {
+				id_max = id
+			}
 		}
-	}
-*/
+	*/
 
 	for {
 		record, err := reader.Read()
@@ -84,82 +85,84 @@ func read_test_ngram(filename string, n int) PairList {
 			break
 		} else if err != nil {
 			fmt.Println("Error:", err)
-      return PairList{}
+			return PairList{}
 		}
 		// record is []string
-    
+
 		//id, _ := strconv.Atoi(record[0])
-    txt   := record[1]
-    
+		txt := record[1]
+
 		//fmt.Println("id,txt:", id, txt)
-    
+
 		words := strings.Split(txt, " ")
-    if 1==n {
-      for i:=0; i<len(words); i++ {
-        word := words[i]
-        //fmt.Println("word:", word)
-        vocab[word]++
-      }
-    }
-
-    if 2==n {
-      words[0] = strings.ToLower(words[0])
-      for i:=0; i<len(words)-1; i++ {
-        word := words[i] + "-" + words[i+1]
-        //fmt.Println("word:", word)
-        vocab[word]++
-      }
-    }
-    
-    /*
-		if id_map[id] {
-			//fmt.Println(record) // record has the type []string
-			
-			steps:=0
-			var data []string
-			
-			if has_steps {
-				steps, _ = strconv.Atoi(record[1])
-				data = record[2:]
-			} else {
-				data = record[1:]
+		if 1 == n {
+			for i := 0; i < len(words); i++ {
+				word := words[i]
+				//fmt.Println("word:", word)
+				vocab[word]++
 			}
-
-			start := NewBoard_BoolPacked(board_width, board_height)
-			end := NewBoard_BoolPacked(board_width, board_height)
-			if is_training {
-				start.LoadArray(data[0:400])
-				end.LoadArray(data[400:800])
-			} else {
-				end.LoadArray(data[0:400])
-			}
-
-			s.problem[id] = LifeProblem{
-				id:    id,
-				start: start,
-				end:   end,
-				steps: steps,
-			}
-			fmt.Printf("Loaded problem[%d] : steps=%d\n", id, steps)
-			//fmt.Print(s.problem[id].start)
 		}
-		if id > id_max {
-			return // fact-of-life : ids are ascending order, so can quit reading early
+
+		if 2 == n {
+			words[0] = strings.ToLower(words[0])
+			for i := 0; i < len(words)-1; i++ {
+				word := words[i] + "-" + words[i+1]
+				//fmt.Println("word:", word)
+				vocab[word]++
+			}
 		}
-    */
+
+		/*
+			if id_map[id] {
+				//fmt.Println(record) // record has the type []string
+
+				steps:=0
+				var data []string
+
+				if has_steps {
+					steps, _ = strconv.Atoi(record[1])
+					data = record[2:]
+				} else {
+					data = record[1:]
+				}
+
+				start := NewBoard_BoolPacked(board_width, board_height)
+				end := NewBoard_BoolPacked(board_width, board_height)
+				if is_training {
+					start.LoadArray(data[0:400])
+					end.LoadArray(data[400:800])
+				} else {
+					end.LoadArray(data[0:400])
+				}
+
+				s.problem[id] = LifeProblem{
+					id:    id,
+					start: start,
+					end:   end,
+					steps: steps,
+				}
+				fmt.Printf("Loaded problem[%d] : steps=%d\n", id, steps)
+				//fmt.Print(s.problem[id].start)
+			}
+			if id > id_max {
+				return // fact-of-life : ids are ascending order, so can quit reading early
+			}
+		*/
 	}
-  
-  pl := sortMapByValue(vocab)
-  
-  l := len(pl)
-  fmt.Printf("Test Vocab size : %d\n", l)
-  
-  if(l>25) { l=25 }
-  for i := 0; i<l; i++ {
-    fmt.Printf("%7d -> %7d %s\n", i, pl[i].Value, pl[i].Key)
-  }  
-  
-  return pl
+
+	pl := sortMapByValue(vocab)
+
+	l := len(pl)
+	fmt.Printf("Test Vocab size : %d\n", l)
+
+	if l > 25 {
+		l = 25
+	}
+	for i := 0; i < l; i++ {
+		fmt.Printf("%7d -> %7d %s\n", i, pl[i].Value, pl[i].Key)
+	}
+
+	return pl
 }
 
 func read_train(filename string) PairList {
@@ -171,36 +174,37 @@ func read_train(filename string) PairList {
 		return PairList{}
 	}
 	defer file.Close()
-  
-  scanner := bufio.NewScanner(file)
 
-  for scanner.Scan() {
-    //  fmt.Println(scanner.Text())
-    txt := scanner.Text()
-    for _, word := range strings.Split(txt, " ") {
-      vocab[word]++
-    }
-  }
+	scanner := bufio.NewScanner(file)
 
-  if err := scanner.Err(); err != nil {
-    //log.Fatal(err)
-    fmt.Println(err)
+	for scanner.Scan() {
+		//  fmt.Println(scanner.Text())
+		txt := scanner.Text()
+		for _, word := range strings.Split(txt, " ") {
+			vocab[word]++
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		//log.Fatal(err)
+		fmt.Println(err)
 		return PairList{}
-  }
+	}
 
-  pl := sortMapByValue(vocab)
-  
-  l := len(pl)
-  fmt.Printf("Train Vocab size : %d\n", l)
-  
-  if(l>25) { l=25 }
-  for i := 0; i<l; i++ {
-    fmt.Printf("%7d -> %7d %s\n", i, pl[i].Value, pl[i].Key)
-  }  
-  
-  return pl
+	pl := sortMapByValue(vocab)
+
+	l := len(pl)
+	fmt.Printf("Train Vocab size : %d\n", l)
+
+	if l > 25 {
+		l = 25
+	}
+	for i := 0; i < l; i++ {
+		fmt.Printf("%7d -> %7d %s\n", i, pl[i].Value, pl[i].Key)
+	}
+
+	return pl
 }
-
 
 const currently_running_version int = 1000
 
@@ -223,60 +227,60 @@ func main() {
 	rand.Seed(*seed)
 
 	fmt.Printf("Billion Start : %s\n", time.Now().Format("2006-01-02_15h04m05s"))
-  start := time.Now()
+	start := time.Now()
 
-	fname_test  := "../data/0-orig/test_v2.txt"
+	fname_test := "../data/0-orig/test_v2.txt"
 	fname_train := "../data/0-orig/train_v2.txt"
-  
+
 	if *cmd == "size" {
 		/// ./billion -cmd=size -type=vocab
 		if *cmd_type == "vocab" {
-      // Read in the vocab for test
-      test_pairs  := read_test_ngram(fname_test,1)
-      fmt.Printf("Billion elapsed : %s\n", time.Since(start))
-      
-      train_pairs := read_train(fname_train)
-      fmt.Printf("Billion elapsed : %s\n", time.Since(start))
-      
-      // Create an empty test vocab
-      test_vocab := map[string]int{}
-      for i:=0; i<len(test_pairs); i++ {
-        p := test_pairs[i]
-        test_vocab[p.Key] = 0
-      }
-      
-      // The fill it with train word freqs (where applicable)
-      for i:=0; i<len(train_pairs); i++ {
-        p := train_pairs[i]
-        if _, ok := test_vocab[p.Key]; ok {
-          test_vocab[p.Key] = p.Value
-        }
-      }
-      
-      // Count the non-zero-freq test words
-      nonzero :=0
-      
-      hist_max := 10
-      hist := make([]int,hist_max)
-      
-      for _,v := range test_vocab {
-        if v>0 {
-          nonzero++
-        }
-        if v<hist_max {
-          hist[v]++
-        }
-      }
+			// Read in the vocab for test
+			test_pairs := read_test_ngram(fname_test, 1)
+			fmt.Printf("Billion elapsed : %s\n", time.Since(start))
 
-      fmt.Printf("NonZero   test vocab words : %d\n", nonzero)
-      for i:=0; i<hist_max; i++ {
-        fmt.Printf("%2d occurences in train : %d\n", i, hist[i])
-      }
+			train_pairs := read_train(fname_train)
+			fmt.Printf("Billion elapsed : %s\n", time.Since(start))
+
+			// Create an empty test vocab
+			test_vocab := map[string]int{}
+			for i := 0; i < len(test_pairs); i++ {
+				p := test_pairs[i]
+				test_vocab[p.Key] = 0
+			}
+
+			// The fill it with train word freqs (where applicable)
+			for i := 0; i < len(train_pairs); i++ {
+				p := train_pairs[i]
+				if _, ok := test_vocab[p.Key]; ok {
+					test_vocab[p.Key] = p.Value
+				}
+			}
+
+			// Count the non-zero-freq test words
+			nonzero := 0
+
+			hist_max := 10
+			hist := make([]int, hist_max)
+
+			for _, v := range test_vocab {
+				if v > 0 {
+					nonzero++
+				}
+				if v < hist_max {
+					hist[v]++
+				}
+			}
+
+			fmt.Printf("NonZero   test vocab words : %d\n", nonzero)
+			for i := 0; i < hist_max; i++ {
+				fmt.Printf("%2d occurences in train : %d\n", i, hist[i])
+			}
 		}
-    
+
 		/// ./billion -cmd=size -type=bigrams
 		if *cmd_type == "bigrams" {
-      read_test_ngram(fname_test, 2)
+			read_test_ngram(fname_test, 2)
 		}
 	}
 
